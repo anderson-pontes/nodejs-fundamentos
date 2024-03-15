d9d761c5b9c69632c2e27d024f3446c55c0998bd
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-03-15 19:55:24 -0300

    add rotas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,33 @@
 import http from 'node:http'
 
+//GET, POST, PUT, PATCH, DELETE
+
+//GET => Buscar um recurso do back-end
+//POST => Criar um recurso no back-end
+//PUT => Atualizar um recurso no back-end
+//PATCH => Atualizar uma informação específica de um recurso no back-end
+//DELETE => Deletar um recurso do back-end
+
+const users = []
 const server = http.createServer((req, res) => {
+  const { method, url} = req
+
+  if(method === 'GET' && url === '/users') {
+    return res.end(JSON.stringify(users))
+  }
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Anderson Pontes',
+      email: 'anderson.pontes@email.com'
+    },
+    {
+      id: 2,
+      name: 'Oliver Pontes',
+      email: 'oliver.pontes@email.com'
+    })
+    return res.end('Criação de usuários')
+  }
   return res.end("Hello, World! Testando")
 
 })
